3dc695c8517a1e4b0832211a7eaa2ef95285fb31
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 20:53:07 +0100

    created the program repository in the application layer

---
+++ ProgramApplication.Application/Common/Interfaces/Persistence/IProgramRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProgramApplication.Domain.Program;
+
+namespace ProgramApplication.Application.Common.Interfaces.Persistence
+{
+    public interface IProgramRepository
+    {
+        Program? GetProgramByIdAsync(string id);
+
+        void Add(Program program);
+    }
+}
