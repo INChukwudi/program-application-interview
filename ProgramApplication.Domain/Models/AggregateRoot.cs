c121de824a59426495356fd45a9e93b73284da6b
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 20:46:26 +0100

    adde domain core models

---
+++ ProgramApplication.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgramApplication.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id) { }
+    }
+}
