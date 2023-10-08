9d90fb894733b408fece2229dbc620c7b22d9870
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 20:54:23 +0100

    added the implmentation of the programrepository and registered the dependency

---
+++ ProgramApplication.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProgramApplication.Application.Common.Interfaces.Persistence;
+using ProgramApplication.Infrastructure.Persistence;
+
+namespace ProgramApplication.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IProgramRepository, ProgramRespository>();
+            return services;
+        }
+    }
+}
---
+++ ProgramApplication.Infrastructure/Persistence/ProgramRespository.cs
@@ -0,0 +1,20 @@
+﻿using ProgramApplication.Application.Common.Interfaces.Persistence;
+using ProgramApplication.Domain.Program;
+
+namespace ProgramApplication.Infrastructure.Persistence
+{
+    public class ProgramRespository : IProgramRepository
+    {
+        private readonly List<Program> _programs = new();
+
+        public void Add(Program program)
+        {
+            _programs.Add(program);
+        }
+
+        public Program? GetProgramByIdAsync(string id)
+        {
+            return _programs.SingleOrDefault(p => p.Id.Value.ToString() == id);
+        }
+    }
+}
