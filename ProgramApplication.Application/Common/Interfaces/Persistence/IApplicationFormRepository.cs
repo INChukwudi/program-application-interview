29cca9b3922f7b43b6c49ff13896bdc6a689c405
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 23:42:31 +0100

    added an interface for the ApplicationFormRepository

---
+++ ProgramApplication.Application/Common/Interfaces/Persistence/IApplicationFormRepository.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationFormNameSpace = ProgramApplication.Domain.ApplicationForm;
+
+namespace ProgramApplication.Application.Common.Interfaces.Persistence
+{
+    public interface IApplicationFormRepository
+    {
+        ApplicationFormNameSpace.ApplicationForm? GetApplicationFormByIdAsync(string id);
+
+        void Add(ApplicationFormNameSpace.ApplicationForm applicationForm);
+    }
+}
