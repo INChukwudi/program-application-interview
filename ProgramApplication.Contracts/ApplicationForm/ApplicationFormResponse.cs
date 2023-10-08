0842890b90160de2762d38905b0a53832f7de807
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 23:41:51 +0100

    added request and response contracts for the applicationForm

---
+++ ProgramApplication.Contracts/ApplicationForm/ApplicationFormResponse.cs
@@ -0,0 +1,91 @@
+﻿namespace ProgramApplication.Contracts.ApplicationForm
+{
+    public record ApplicationFormResponse(
+        string Id,
+        CoverImageSectionResponse CoverImageSection,
+        PersonalInformationSectionResponse PersonalInformationSection,
+        ProfileSectionResponse ProfileSection,
+        AdditionalQuestionsSectionResponse AdditionalQuestionsSection);
+
+    public record CoverImageSectionResponse(
+        string Name,
+        FileUploadQuestionResponse Question);
+
+    public record PersonalInformationSectionResponse(
+        string Name,
+        List<QuestionBaseResponse> Questions);
+
+    public record ProfileSectionResponse(List<ProfileSubSectionResponse> SubSections);
+
+    public record AdditionalQuestionsSectionResponse(
+        string Name,
+        List<QuestionBaseResponse> Questions);
+
+    public record ProfileSubSectionResponse(
+        string Name,
+        bool IsMandatory,
+        bool IsHidden,
+        List<QuestionBaseResponse> Questions);
+
+    public record DateQuestionResponse(
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record DropdownQuestionResponse(
+        bool IsOtherEnabled,
+        List<string> Choices,
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record FileUploadQuestionResponse(
+        ushort MaxSize,
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record MultipleChoiceQuestionResponse(
+        bool IsOtherEnabled,
+        List<string> Choices,
+        ushort MaxChoices,
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record NumberQuestionResponse(
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record ParagraphQuestionResponse(
+        uint Limit,
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record ShortAnswerQuestionResponse(
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record YesNoQuestionResponse(
+        bool DisqualifyIfNo,
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false) : QuestionBaseResponse(Question, Type, IsInternal, IsHidden);
+
+    public record QuestionBaseResponse(
+        string Question,
+        string Type,
+        bool IsInternal = false,
+        bool IsHidden = false);
+}
---
+++ ProgramApplication.Contracts/ApplicationForm/CreateApplicationFormRequest.cs
@@ -0,0 +1,69 @@
+﻿namespace ProgramApplication.Contracts.ApplicationForm
+{
+    public record CreateApplicationFormRequest(
+        CoverImageSectionRequest CoverImageSection,
+        PersonalInformationSectionRequest PersonalInformationSection,
+        ProfileSectionRequest ProfileSection,
+        AdditionalQuestionsSectionRequest AdditionalQuestionsSection);
+
+    public record CoverImageSectionRequest(
+        string Name,
+        FileUploadQuestionRequest Question);
+    
+    public record PersonalInformationSectionRequest(
+        string Name,
+        List<QuestionBaseRequest> Questions);
+    
+    public record ProfileSectionRequest(List<ProfileSubSectionRequest> SubSections);
+    
+    public record AdditionalQuestionsSectionRequest(
+        string Name,
+        List<QuestionBaseRequest> Questions);
+    
+    public record ProfileSubSectionRequest(
+        string Name,
+        bool IsMandatory,
+        bool IsHidden,
+        List<QuestionBaseRequest> Questions);
+
+    public record DateQuestionRequest(QuestionBaseRequest QuestionBase);
+    
+    public record DropdownQuestionRequest(
+        bool IsOtherEnabled,
+        List<string> Choices,
+        QuestionBaseRequest QuestionBase);
+
+    public record FileUploadQuestionRequest(
+        ushort MaxSize,
+        QuestionBaseRequest QuestionBase);
+
+    public record MultipleChoiceQuestionRequest(
+        bool IsOtherEnabled,
+        List<string> Choices,
+        ushort MaxChoices,
+        QuestionBaseRequest QuestionBase);
+    
+    public record NumberQuestionRequest(QuestionBaseRequest QuestionBase);
+    
+    public record ParagraphQuestionRequest(
+        uint Limit,
+        QuestionBaseRequest QuestionBase);
+    
+    public record ShortAnswerQuestionRequest(QuestionBaseRequest QuestionBase);
+    
+    public record YesNoQuestionRequest(
+        bool DisqualifyIfNo,
+        QuestionBaseRequest QuestionBase);
+
+    public record QuestionBaseRequest(
+        string Question,
+        string Type,
+        uint Limit,
+        ushort MaxSize,
+        bool IsOtherEnabled,
+        List<string> Choices,
+        ushort MaxChoices,
+        bool DisqualifyIfNo = false,
+        bool IsInternal = false,
+        bool IsHidden = false);
+}
