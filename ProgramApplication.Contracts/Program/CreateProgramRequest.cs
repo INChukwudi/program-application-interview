eebd1b55740ee25f893773c233f2af092930b7dc
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-08 20:59:43 +0100

    added contracts for the program requests and responses

---
+++ ProgramApplication.Contracts/Program/CreateProgramRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace ProgramApplication.Contracts.Program
+{
+    public record CreateProgramRequest(
+        string Title,
+        string Summary,
+        string Description,
+        List<string> KeySkills,
+        string Benefits,
+        string Criteria,
+        string Type,
+        DateTimeOffset StartDate,
+        DateTimeOffset ApplicationOpen,
+        DateTimeOffset ApplicationClose,
+        ushort Duration,
+        string Location,
+        string MinQualification,
+        uint MaxApplications
+    );
+}
---
+++ ProgramApplication.Contracts/Program/ProgramResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace ProgramApplication.Contracts.Program
+{
+    public record ProgramResponse(
+        string Id,
+        string Title,
+        string Summary,
+        string Description,
+        List<string> KeySkills,
+        string Benefits,
+        string Criteria,
+        string Type,
+        DateTimeOffset StartDate,
+        DateTimeOffset ApplicationOpen,
+        DateTimeOffset ApplicationClose,
+        ushort Duration,
+        string Location,
+        string MinQualification,
+        uint MaxApplications,
+        DateTimeOffset CreatedAt,
+        DateTimeOffset LastModifiedAt,
+        string ApplicationFormId,
+        string WorkFlowId);
+}
