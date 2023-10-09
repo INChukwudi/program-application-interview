621bab553fdcfc245339be3ddab66a49fa93dec0
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-09 07:06:11 +0100

    added the workflow repository interface

---
+++ ProgramApplication.Application/Common/Interfaces/Persistence/IWorkflowRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProgramApplication.Domain.Workflow;
+
+namespace ProgramApplication.Application.Common.Interfaces.Persistence
+{
+    public interface IWorkflowRepository
+    {
+        WorkFlow? GetWorflowById(string id);
+
+        void Add(WorkFlow workFlow);
+    }
+}
