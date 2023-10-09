40e71bcb0ece69a120d5b78228a54a67886d7f86
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-09 07:09:49 +0100

    added the create workflow command and its command handler

---
+++ ProgramApplication.Application/Workflow/Commands/CreateWorkflow/CreateWorkflowCommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+using ProgramApplication.Domain.Workflow;
+using ProgramApplication.Domain.Workflow.Enitites.StageEntities;
+
+namespace ProgramApplication.Application.Workflow.Commands.CreateWorkflow
+{
+    public record CreateWorkflowCommand(List<Stage> Stages) : IRequest<ErrorOr<WorkFlow>>;
+}
---
+++ ProgramApplication.Application/Workflow/Commands/CreateWorkflow/CreateWorkflowCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using ErrorOr;
+using MediatR;
+using ProgramApplication.Application.Common.Interfaces.Persistence;
+using ProgramApplication.Domain.Workflow;
+
+namespace ProgramApplication.Application.Workflow.Commands.CreateWorkflow
+{
+    public class CreateWorkflowCommandHandler : IRequestHandler<CreateWorkflowCommand, ErrorOr<WorkFlow>>
+    {
+        private readonly IWorkflowRepository _workflowRepository;
+
+        public CreateWorkflowCommandHandler(IWorkflowRepository workflowRepository)
+        {
+            _workflowRepository = workflowRepository;
+        }
+
+        public async Task<ErrorOr<WorkFlow>> Handle(CreateWorkflowCommand request, CancellationToken cancellationToken)
+        {
+            var workflow = WorkFlow.Create();
+            workflow.AddStages(request.Stages);
+
+            _workflowRepository.Add(workflow);
+
+            return workflow;
+        }
+    }
+}
