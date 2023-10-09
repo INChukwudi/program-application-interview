b4a52f8a72af75d8821d66daf9a699520a22ca8b
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-09 10:04:15 +0100

    added the cosmos db configuration and registered the cosmosclient as a a singleton

--- ProgramApplication.Infrastructure/DependencyInjection.cs
+++ ProgramApplication.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
 using ProgramApplication.Application.Common.Interfaces.Persistence;
 using ProgramApplication.Infrastructure.Persistence;
 
@@ -6,7 +7,31 @@ namespace ProgramApplication.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureDependencies
+            (this IServiceCollection services)
+        {
+            services.AddRepositoryDependencies();
+            services.AddCosmosDependencies();
+            return services;
+        }
+        
+        public static IServiceCollection AddCosmosDependencies
+            (this IServiceCollection services)
+        {
+            services.AddSingleton(() =>
+            {
+                var cosmosClientOptions = new CosmosClientOptions
+                {
+                    ApplicationName = CosmosConfig.DatabaseName
+                };
+                var cosmosClient = new CosmosClient(CosmosConfig.ConnectionString, cosmosClientOptions);
+                cosmosClient.ClientOptions.ConnectionMode = ConnectionMode.Direct;
+                return cosmosClient;
+            });
+            return services;
+        }
+        
+        public static IServiceCollection AddRepositoryDependencies(this IServiceCollection services)
         {
             services.AddScoped<IProgramRepository, ProgramRespository>();
             services.AddScoped<IApplicationFormRepository, ApplicationFormRepository>();
---
+++ ProgramApplication.Infrastructure/Persistence/CosmosConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ProgramApplication.Infrastructure.Persistence
+{
+    public static class CosmosConfig
+    {
+        public const string EndpointUrl = "https://localhost:8081";
+        public const string DatabaseName = "ProgramApplicationDb";
+        public const string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        public const string ConnectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+    }
+}
